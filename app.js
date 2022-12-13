0326655c1d553d0ce1c43f77474ce3e75a4d53d1
Paula Guevara <paula.guevara91@gmail.com>
2022-12-12 22:39:15 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,76 @@
+/*primer declaro las dos variables que tiene mi mazo de cartas*/
+const PALOS = ["♠", " ♥", "♦", "♣ "];
+
+const VALORES = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+/*creo la funcion para crear el mazo, utilicé flatmap ya que map solo me creaba 4 arrays diferentes, uno por cada palo*/
+function crearMazoDeCartas() {
+    return PALOS.flatMap (palo =>  {
+        return VALORES.map (valor => {
+            return new Carta(palo, valor);
+        })
+    })
+}
+/*creo la clase carta*/
+class Carta {
+    constructor(palo, valor){
+        this.palo = palo;
+        this.valor = valor;
+    }
+}
+
+class MazoDeCartas{
+    constructor(cartas = crearMazoDeCartas()){
+    this.cartas = cartas;
+    }
+
+    get cantidadDeCartas() {
+        return this.cartas.length;
+    }
+
+    mezclar(){
+        for (let i = this.cantidadDeCartas - 1 ; i > 0; i--) {
+            const mazoMezclado = Math.floor(Math.random() * (i + 1));
+            const mazoSinMezclar = this.cartas[mazoMezclado];
+            this.cartas[mazoMezclado] = this.cartas[i];
+            this.cartas[i] = mazoSinMezclar;
+        }
+        return mazo;
+    }
+    repartir(){
+        return this.cartas.pop();
+    }
+}
+
+const mazo = new MazoDeCartas;
+console.log(mazo.mezclar());
+
+const baraja = JSON.stringify(mazo);
+
+function iniciarPartida(){
+    hidden = mazo.repartir();
+    let cardImg = document.createElement("img")
+    let carta = mazo.repartir();
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
